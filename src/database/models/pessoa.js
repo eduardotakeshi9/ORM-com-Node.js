99d20ac571c566e1aa3087ec67b85513f0cb1610
Eduardo Takeshi <132021455+eduardotakeshi9@users.noreply.github.com>
2023-12-13 13:31:04 -0300

    Add files via upload

---
+++ src/database/models/pessoa.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    static associate(models) {
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: 'docente_id'
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        scope: { status: 'matriculado' },
+        as: 'aulasMatriculadas'
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        as: 'todasAsMatriculas'
+      });
+    }
+  }
+  Pessoa.init({
+    nome: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: { args: true, msg: 'Formado do email invalido' }
+      }
+    },
+    cpf: DataTypes.STRING,
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+    tableName: 'pessoas',
+    paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true,
+      }
+    },
+    scopes: {
+      todosOsRegistros: {
+        where: {}
+      }
+    }
+  });
+  return Pessoa;
+};
